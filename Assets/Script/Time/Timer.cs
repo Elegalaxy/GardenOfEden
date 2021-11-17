048421eafe2684ab63f5e53e3029ec1df9ebe7d3
ZeKai Chong <yokai.czk@gmail.com>
2021-11-18 08:22:44 +1030

    18 Nov 2021
    - Create time

---
+++ Assets/Script/Time/Timer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    float real = 0f;
+    float now = 0f;
+
+    float getNowTime(float t) { //1s in world = 720s in Eden
+        return t*720;
+    }
+
+    void Update()
+    {
+        real += Time.deltaTime;
+
+        if(real >= 120)
+            real = 0;
+
+        now = getNowTime(real);
+
+        Debug.Log((int)(now/3600)); //Hour
+    }
+}
