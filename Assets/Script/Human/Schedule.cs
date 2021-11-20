91cd8d11227f62045b17f029e776aa4f6abdd757
ZeKai Chong <yokai.czk@gmail.com>
2021-11-20 14:07:52 +1030

    20 Nov 2021
    - Fix schedule

--- Assets/Script/Human/Schedule.cs
+++ Assets/Script/Human/Schedule.cs
@@ -29,7 +29,9 @@ public class Schedule : MonoBehaviour
         {
             GameObject[] workplace = GameObject.FindGameObjectsWithTag("Maker");
             workplace[0].GetComponent<Maker>().work(GetComponent<Human>().get_Stat());
-            Debug.Log("Working"+workplace[0]);
+            Debug.Log("Working"+workplace[0].GetComponent<Maker>().get_name());
+        } else {
+            Debug.Log("Free");
         }
 
     }
