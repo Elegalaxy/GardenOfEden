2f47a74ba0432146d4c1bacebe127275e698f1fb
ZeKai Chong <yokai.czk@gmail.com>
2021-11-20 12:39:13 +1030

    20 Nov 2021
    - Fix bugs

--- Assets/Script/Maker/Seller.cs
+++ Assets/Script/Maker/Seller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Seller: Maker {
-    public Seller(string n = "", int l = 0, int t = 0, int s, Item[] i = null) : base(n, 3, l, t, s, i) {
+    public Seller(string n = "", int l = 0, int t = 0, int s = 0, Item[] i = null) : base(n, 3, l, t, s, i) {
         
     }
 
